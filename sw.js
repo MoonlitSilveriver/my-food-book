773c6b95b3ccc3a5ab6519d9726bc7c32b6dbc2e
Silveriver <2798253498@qq.com>
2026-01-09 13:22:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = 'baby-food-v1000'; // 版本号改大一点
+const CACHE_NAME = 'baby-food-v1001'; // 版本号改大一点
 const urlsToCache = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
@@ -10,4 +10,5 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
+
 
